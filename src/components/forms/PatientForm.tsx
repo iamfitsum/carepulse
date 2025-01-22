6d0c220803449eac019e64ee40ff87e555e965b9
iamfitsum <official.fitsum.mekonnen@gmail.com>
2025-01-22 23:47:01 +0300

    Add create patient server action

--- src/components/forms/PatientForm.tsx
+++ src/components/forms/PatientForm.tsx
@@ -11,8 +11,11 @@ import { UserFormValidation } from "@/lib/validation";
 import "react-phone-number-input/style.css";
 import CustomFormField, { FormFieldType } from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
+import { createUser } from "@/lib/actions/patient.actions";
+import { useRouter } from "next/navigation";
 
 const PatientForm = () => {
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm<z.infer<typeof UserFormValidation>>({
@@ -33,10 +36,13 @@ const PatientForm = () => {
         email: values.email,
         phone: values.phone,
       };
+      
+      const newUser = await createUser(user);
 
-      console.log(user);
-
-      // TODO: Implement Create User
+      if (newUser) {
+        router.push(`/patients/${newUser.$id}/register`);
+      }
+      
     } catch (error) {
       console.log(error);
     }
@@ -49,9 +55,7 @@ const PatientForm = () => {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
         <section className="mb-12 space-y-4">
           <h1 className="header">Hi there 👋</h1>
-          <p className="text-dark-700">
-            Schedule your first appointment.
-          </p>
+          <p className="text-dark-700">Schedule your first appointment.</p>
         </section>
         <CustomFormField
           fieldType={FormFieldType.INPUT}
